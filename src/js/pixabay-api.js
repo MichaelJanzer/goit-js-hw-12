677df487e8ebafd26236db794d653670ed0cbabc
Michael Janzer <michaeljanzer1996@gmail.com>
2024-07-19 20:36:58 +0300

    14

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -25,23 +25,3 @@ export async function getPicturesByQuery(imageName, page = 1) {
 
 
 
-// import { formInput, showLoader } from '../main';
-
-// export function getPicturesByQuery() {
-//   const query = formInput.value;
-//   console.log(query);
-//   const API_KEY = '44869216-4addd85d29d39c45ae242764b';
-//   return fetch(
-//     `https://pixabay.com/api/?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`
-//   ).then(res => {
-    
-//     console.log(res);
-
-   
-//     if (!res.ok) {
-//       throw new Error(res.status); 
-//     }
-
-   
-//     return res.json();
-//   });
